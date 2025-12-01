4464862ee1183e5977391db7328024ddae21666c
Dorian Jacolin <dorian.jacolin@my-digital-school.org>
2025-12-01 15:46:50 +0100

    getData

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { use, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,25 +6,30 @@ import './App.css'
 
 function App() {
   const [pokemonList, setPokemonList] = useState([]);
-
-  //  useEffect(() => {
-  //    const getData = async () => {
-  //      const res = await fetch("http://localhost:1337/api/pokemons/")
-  //      const data = await res.json()
-  //      console.log('ressssssss', res)
-  //      setPokemonList(data.data);
-  //    }
-  //    getData();
-  //  }, [])
-
-   const randomNumber = Math.floor(Math.random() * 151) + 1;
-   console.log('randomNumber', randomNumber);
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('http://localhost:1337/api/pokemons');
+      const data = await response.json();
+      console.log(data);
+      setPokemonList(data.results);
+      console.log("pokemonList", pokemonList);
+    }
+    getData();
+  }, []); 
 
   return(
     <div className="container-app">
       <h1>
-        random number: {randomNumber}
+        Pokemon List
       </h1>
+      <div className="pokemon-cards">
+        {pokemonList.map((pokemon, index) => (
+          <div key={index} className="pokemon-card">
+            <h2>{pokemon.name}</h2>
+            <img src={pokemon.sprite} alt={pokemon.name} />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
