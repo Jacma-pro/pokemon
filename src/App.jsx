f69071650f67cde472ef767d182c6debb65efaef
Dorian Jacolin <dorian.jacolin@my-digital-school.org>
2025-12-01 14:19:20 +0100

    add poke1

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,24 @@ import './App.css'
 function App() {
   const [pokemonList, setPokemonList] = useState([]);
 
-   useEffect(() => {
-     const getData = async () => {
-       const res = await fetch("http://localhost:1337/api/pokemons/")
-       const data = await res.json()
-       console.log('ressssssss', res)
-       setPokemonList(data.data);
-     }
-     getData();
-   }, [])
+  //  useEffect(() => {
+  //    const getData = async () => {
+  //      const res = await fetch("http://localhost:1337/api/pokemons/")
+  //      const data = await res.json()
+  //      console.log('ressssssss', res)
+  //      setPokemonList(data.data);
+  //    }
+  //    getData();
+  //  }, [])
+
+   const randomNumber = Math.floor(Math.random() * 151) + 1;
+   console.log('randomNumber', randomNumber);
 
   return(
     <div className="container-app">
-      <ul>
-        {pokemonList.map((pokemon, index) => (
-          <li key={index}>{pokemon.name}</li>
-        ))}
-      </ul>
+      <h1>
+        random number: {randomNumber}
+      </h1>
     </div>
   )
 }
