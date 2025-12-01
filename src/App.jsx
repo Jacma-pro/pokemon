005dc1cbdd48246f0f9c9f5af6d8fa36ee951777
Dorian Jacolin <dorian.jacolin@my-digital-school.org>
2025-12-01 16:07:15 +0100

    pokemonList

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,47 @@ import './App.css'
 
 function App() {
   const [pokemonList, setPokemonList] = useState([]);
+
+  console.log("pokemonList", pokemonList);
+
+
   useEffect(() => {
     const getData = async () => {
       const response = await fetch('http://localhost:1337/api/pokemons');
       const data = await response.json();
-      console.log(data);
-      setPokemonList(data.results);
-      console.log("pokemonList", pokemonList);
+      console.log("get initial data", data);
+      setPokemonList(data.data);
     }
     getData();
   }, []); 
 
-  return(
-    <div className="container-app">
-      <h1>
-        Pokemon List
-      </h1>
+  const handleOnOrTwoTypes = () => {;
+    if (pokemonList.type2) {
+      return (
+        <p>Type 1: {pokemonList.type1} | Type 2: {pokemonList.type2}</p>
+      )
+    } else {
+      return (
+        <p>Type: {pokemonList.type1}</p>
+      )
+    }
+  }
+    return(
+      <div className="container-app">
+        <h1>
+          Pokemon List
+        </h1>
       <div className="pokemon-cards">
         {pokemonList.map((pokemon, index) => (
           <div key={index} className="pokemon-card">
-            <h2>{pokemon.name}</h2>
-            <img src={pokemon.sprite} alt={pokemon.name} />
+            <div className="top">
+              <h2>{pokemon.name}</h2>
+              <img src={pokemon.sprite} alt={pokemon.name} />
+            </div>
+            <div className="bottom">
+              {handleOnOrTwoTypes(pokemon)}
+              <p>HP: {pokemon.hp}</p> 
+            </div>
           </div>
         ))}
       </div>
